63e10c5e70e4cb9d0acf9e0fc5d24cb9fa065f33
anshshah91 <govardhanshah456@gmail.com>
2021-03-08 19:55:31 +0530

    commit

--- ecomsite/models/Product.py
+++ ecomsite/models/Product.py
@@ -7,3 +7,15 @@ class Product(models.Model):
     fk=models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
     Description=models.CharField(max_length=200,default='')
     Image=models.ImageField(upload_to='uploads/products/')
+    @staticmethod
+    def get_product_by_id(ids):
+        return Product.objects.filter(id__in=ids)
+    @staticmethod
+    def get_all_Products():
+        return Product.objects.all()
+    @staticmethod
+    def get_all_Products_byid(fk_id):
+        if fk_id:
+            return Product.objects.filter(fk=fk_id)
+        else:
+            return Product.get_all_Products()
